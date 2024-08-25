cf21376db36a51501f3575ba1e4cee9979d26382
Fischer Zhang <zzzha5@student.unimelb.edu.au>
2024-08-25 20:09:02 +1000

    #3 routes, utils and controllers modules are created, and some basic routing .js files are added

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,31 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// Importing route handlers
+const accountRoutes = require('./routes/accountRoutes');
+const transactionRoutes = require('./routes/transactionRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const billRoutes = require('.routes/adminRoutes');
+const { ScheduleRecurringPayments } = require('.utils/scheduler');
+
 dotenv.config();
 
 const app = express();
 
 // Middleware to parse JSON requests
 app.use(express.json());
+
+// Logger middleware to track incoming requests
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// Route handlers
+app.use('/api/accounts', accountRoutes);
+app.use('/api/transactions', transactionRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/bills', billRoutes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
